6ccd9a5eb3d82e6fcc78e573fcb8a8002a14a7ba
PATMESH <101879714+PATMESH@users.noreply.github.com>
2024-01-18 21:59:39 +0530

    updated

--- frontend/src/components/SelectLanguage.jsx
+++ frontend/src/components/SelectLanguage.jsx
@@ -1,34 +1,64 @@
-import React from 'react';
-import CustomNavbar from './LandingPage/Navbar';
-
-const languages = [
-  { id: 1, name: 'English' },
-  { id: 2, name: 'French' },
-  { id: 3, name: 'German' },
-];
+import React, { useState } from "react";
+import CustomNavbar from "./LandingPage/Navbar";
+import logo from "./img/translator.png";
 
 const SelectLanguage = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState(null);
+
+  const languages = [
+    { id: 1, name: "English" },
+    { id: 2, name: "French" },
+    { id: 3, name: "German" },
+    { id: 4, name: "Spanish" },
+    { id: 5, name: "Italian" },
+    { id: 6, name: "Japanese" },
+    { id: 7, name: "Chinese" },
+    { id: 8, name: "Russian" },
+    { id: 9, name: "Arabic" },
+    { id: 10, name: "Portuguese" },
+    { id: 11, name: "Dutch" },
+    { id: 12, name: "Korean" },
+  ];
+
   const handleLanguageSelect = (language) => {
-    console.log(`Selected language: ${language.name}`);
+    setSelectedLanguage(language.name);
   };
 
   return (
     <>
       <CustomNavbar />
       <div className="select-language-container">
+        <div className="img-logo-container">
+          <img src={logo}></img>
+        </div>
         <h1>Select Your Language</h1>
         <div className="language-list">
           {languages.map((language) => (
             <div
               key={language.id}
-              className="language-item"
+              className={`language-item ${
+                selectedLanguage === language.name ? "selected-language" : ""
+              }`}
               onClick={() => handleLanguageSelect(language)}
             >
               {language.name}
             </div>
           ))}
         </div>
-        <button className="start-learning-btn">Start Learning</button>
+        {selectedLanguage ? (
+          <button
+            className="start-learning-btn"
+            onClick={() =>
+              console.log(`Start learning ${selectedLanguage.name}`)
+            }
+          >
+            Start Learning
+          </button>
+        ) : (
+          <button className="disabled-learning-btn" disabled>
+            Select a Language
+          </button>
+        )}
       </div>
     </>
   );
