217197ece5738166420f403f81148e270ee5decb
PATMESH <101879714+PATMESH@users.noreply.github.com>
2024-01-19 21:13:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/LandingPage/Home";
 import Login from "./components/Login";
-import UserProfile from "./components/UserProfile";
 import RegistrationForm from "./components/Register";
 import SelectLanguage from "./components/SelectLanguage";
 import LanguageProfile from "./components/LanguageProfile";
@@ -24,10 +23,6 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login setUser={setUser} />}></Route>
           <Route path="/register" element={<RegistrationForm />}></Route>
-          <Route
-            path="/user-profile"
-            element={<UserProfile user={user} />}
-          ></Route>
           <Route
             path="/LanguageProfile/:language"
             element={<LanguageProfile />}
